3c07d4fe457f831839e016b28ba78d783b7020c2
gilberthsolano <49965038+gilberthsolano@users.noreply.github.com>
2024-11-19 22:27:02 -0500

    first commit add

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -36,7 +36,7 @@ const HomePage = () => {
         <div className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
           <img
             alt="Descripción de la imagen"
-            src="/chio.jpg"  // Ruta de la imagen en la carpeta public
+            src="\dist\chio.jpg"  // Ruta de la imagen en la carpeta public
             className="w-full h-auto max-w-none rounded-xl object-cover bg-gray-900 shadow-xl ring-1 ring-gray-400/10"
           />
         </div>
